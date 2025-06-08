8931a04a25a64c79aa44faa50474d41c56ded819
Goldmite <aurijus.sus@gmail.com>
2025-06-08 13:50:03 +0300

    Fixed schema wrong paranthesis

--- internal/database/schema.sql
+++ internal/database/schema.sql
@@ -18,10 +18,10 @@ CREATE TABLE IF NOT EXISTS books (
     language INTEGER NOT NULL CHECK (language BETWEEN 0 AND 2)
 );
 
-CREATE TABLE IF NOT EXISTS views {
+CREATE TABLE IF NOT EXISTS views (
     user_id TEXT,
     book_id TEXT,
     PRIMARY KEY (user_id, book_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(isbn) ON DELETE CASCADE
-};
+);
