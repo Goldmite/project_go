cd951a55488b76f2a2e0b068b5cc838f58837ee2
Goldmite <aurijus.sus@gmail.com>
2025-06-09 12:02:37 +0300

    Base structure, main page WIP

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav class="flex p-10 justify-between">
+    <img class="h-16 w-auto" src="./SHELF_LOGO.svg" alt="Shelf" >
+    <a href="/login">
+        <button class="bg-blue-300 p-4 rounded-lg"> Log In </button>
+    </a>
+</nav>
+
+<div class="flex flex-col items-center justify-center h-screen prose">
+    <h1>Welcome to your digital shelf</h1>
+    <p>Digitalize your bookshelf, track progress, make a group - share your shelves.</p>
+</div>
+
