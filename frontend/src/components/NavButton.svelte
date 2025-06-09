6d639fc03871bf31258cb539a5a4ab70ee22e0c2
Goldmite <aurijus.sus@gmail.com>
2025-06-09 23:25:13 +0300

    Prettier styling

--- frontend/src/components/NavButton.svelte
+++ frontend/src/components/NavButton.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-    let {link, buttonText, color} = $props();
+	let { link, buttonText, color } = $props();
 </script>
-<a href="{link}">
-    <!-- Safelist: text-logo-blue shadow-logo-red shadow-logo-blue -->
-    <button class="text-{color} bg-neutral font-sans font-bold rounded-xl shadow-md hover:shadow-lg shadow-{color} p-3 m-2 "> {buttonText} </button>
+
+<a href={link}>
+	<!-- Safelist: text-logo-blue shadow-logo-red shadow-logo-blue -->
+	<button
+		class="text-{color} bg-neutral rounded-xl font-sans font-bold shadow-md hover:shadow-lg shadow-{color} m-2 p-3"
+	>
+		{buttonText}
+	</button>
 </a>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,25 +1,24 @@
-<svelte:head>
-	<script>
-		// Apply saved theme before app renders
-		const theme = localStorage.getItem('theme');
-		if (theme === 'dark') {
-		document.documentElement.classList.add('dark');
-		}
-</script>
-</svelte:head>
-
 <script lang="ts">
 	import '../app.css';
-	import DarkToggle from "../components/DarkToggle.svelte";
+	import DarkToggle from '../components/DarkToggle.svelte';
 
 	let { children } = $props();
 </script>
 
-<nav class="flex p-4 items-center justify-between">
-    <div class="flex items-center space-x-4">
-        <img class="h-10 w-auto" src="./SHELF_LOGO.svg" alt="Logo" >
-        <h1 class="text-3xl font-bold">Shelf</h1>
-    </div>
-    <DarkToggle> </DarkToggle>
+<svelte:head>
+	<script>
+		const theme = localStorage.getItem('theme');
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		}
+	</script>
+</svelte:head>
+
+<nav class="flex items-center justify-between p-4">
+	<div class="flex items-center space-x-4">
+		<img class="h-10 w-auto" src="./SHELF_LOGO.svg" alt="Logo" />
+		<h1 class="text-3xl font-bold">Shelf</h1>
+	</div>
+	<DarkToggle></DarkToggle>
 </nav>
 {@render children()}
