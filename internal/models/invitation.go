d6e16fe76bc20aab5a70c42ab91cbd07a1c33c38
Goldmite <aurijus.sus@gmail.com>
2025-06-17 15:05:55 +0300

    Added create group and invite queries

--- internal/models/invitation.go
+++ internal/models/invitation.go
@@ -1,19 +1,39 @@
 package models
 
-import "github.com/Goldmite/project_go/internal/enums"
+import (
+	"time"
+
+	"github.com/Goldmite/project_go/internal/enums"
+	"github.com/google/uuid"
+)
 
 type Invitation struct {
-	token     string `form:"token"`
-	emailTo   string `form:"email_to"`
-	groupId   string `form:"group_id"`
-	invitedBy string `form:"invited_by"`
-	status    enums.Status
-	sentAt    string
-	expiresAt string
+	Token     string `form:"token"`
+	EmailTo   string `form:"email_to"`
+	GroupId   string `form:"group_id"`
+	InvitedBy string `form:"invited_by"`
+	Status    enums.Status
+	SentAt    string
+	ExpiresAt string
 }
 
 type InviteRequest struct {
-	token     string `json:"token"`
-	groupId   string `json:"group_id"`
-	invitedBy string `json:"invited_by"`
+	EmailTo   string `json:"email_to`
+	GroupId   string `json:"group_id"`
+	InvitedBy string `json:"invited_by"`
+}
+
+func NewInvitationFromRequest(r InviteRequest) *Invitation {
+	sentAt := time.Now().String()
+	expiresAt := time.Now().Add(time.Hour * 24 * 7).String() // expire after a week
+
+	return &Invitation{
+		Token:     uuid.New().String(),
+		EmailTo:   r.EmailTo,
+		GroupId:   r.GroupId,
+		InvitedBy: r.InvitedBy,
+		Status:    enums.Pending,
+		SentAt:    sentAt,
+		ExpiresAt: expiresAt,
+	}
 }
---
+++ internal/services/group_service.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Goldmite/project_go/internal/models"
+)
+
+type GroupService struct {
+	database *sql.DB
+}
+
+func NewGroupService(db *sql.DB) *GroupService {
+	return &GroupService{database: db}
+}
+
+func (groupService *GroupService) CreateGroup(name string) (*string, error) {
+	g := models.NewGroupFromName(name)
+	query := "INSERT INTO groups (id, name, created_at) VALUES (?, ?, ?)"
+	_, err := groupService.database.Exec(query, g.Id, g.Name, g.CreatedAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create group %w", err)
+	}
+
+	return &g.Id, nil
+}
+
+func (groupService *GroupService) CreateInvite(req models.InviteRequest) error {
+	inv := models.NewInvitationFromRequest(req)
+	query := "INSERT INTO invitations (token, email_to, group_id, invited_by, status, sent_at, expires_at) VALUES (?, ?, ?, ?, ?, ?, ?)"
+	_, err := groupService.database.Exec(query, inv.Token, inv.EmailTo, inv.GroupId, inv.InvitedBy, inv.Status, inv.SentAt, inv.ExpiresAt)
+	if err != nil {
+		return fmt.Errorf("failed to create invite %w", err)
+	}
+
+	return nil
+}
